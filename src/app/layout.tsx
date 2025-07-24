0f9df27e80a4526c82c26203ac6ed3e2a946b8a4
Ajith <hello@stikkmanux.com>
2025-07-24 18:09:30 +0530

    font fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Basic&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
